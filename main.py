c7a2942a89aeda5cc5569bcbd11ae77d60d35de3
R3dChet <Vendigo331@gmail.com>
2020-11-16 20:29:40 +0700

    Fixed bug with incorrect display of damage испрален баг неправильного отображения урона

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ hp = [randint(20, 30), randint(20, 30)]  # Количество здоровья
 strength = [1, 1]  # сила
 dexterity = [2, 2]  # ловкость
 att = [[1, 6], [1, 6]]  # количество кубов, наминал
-add_damage = [5, 5]  # + к урону
+add_damage = [1, 1]  # + к урону
 add_hit = [5, 5]  # + к попаданию
 
 gork = Enemy('Горк', kd[0], hp[0], strength[0], dexterity[0], att[0], add_damage[0], add_hit[0])
