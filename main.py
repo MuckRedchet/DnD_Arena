1d790a361e839b6f0f0aff3d3c9a8f25f10b9589
R3dChet <Vendigo331@gmail.com>
2020-12-01 13:37:26 +0700

    Variability of death Вариотивность смерти

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from enemy import Enemy
-from fight import fight
+from fight import Fight
 from random import randint
 
 kd = [17, 18]  # коофициент доспеха
-hp = [randint(10, 11), randint(10, 11)]  # Количество здоровья
+hp = [randint(50, 51), randint(50, 51)]  # Количество здоровья
 strength = [6, 6]  # сила
 dexterity = [3, 1]  # ловкость
 att = [[3, 10], [4, 6]]  # количество кубов, наминал
@@ -13,4 +13,6 @@ add_hit = [1, 1]  # + к попаданию
 gork = Enemy('Горк', kd[0], hp[0], strength[0], dexterity[0], att[0], add_damage[0], add_hit[0])
 mork = Enemy('Морк', kd[1], hp[1], strength[1], dexterity[1], att[1], add_damage[1], add_hit[1])
 
-fight(gork, mork)
+fight = Fight(gork, mork)
+
+fight.fight()
