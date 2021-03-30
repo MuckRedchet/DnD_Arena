15085dbb5e2c833b76519191af3a58aa28691828
R3dChet <Vendigo331@gmail.com>
2021-03-30 21:16:35 +0700

    Refactoring all cycles in the 'Enemy' class. Return directly from the condition in the 'ENEMY' class. Correction class 'Fight' - Now the opponents beat each other in turn as it should be. Dictionaries instead of lists for readability of the code.
    Реффакторинг всех циклов в классе 'Enemy'.
    return прямо из условия в классе 'Enemy'.
    Исправление класса 'Fight' - теперь противники бьют друг друга по очереди как и положено.
    Словари вместо списков для удобочитаемости кода.

--- enemy.py
+++ enemy.py
@@ -13,28 +13,24 @@ class Enemy:
         self.add_hit = add_hit  # + к попаданию
 
     def attack(self):
-        dice_roll = randint(1, 20)
-        dice_roll_attack = dice_roll + self.strength + self.add_hit
-        attack_m = self.att
+        dice_roll = randint(1, 20) # Бросок двацатки
+        dice_roll_attack = dice_roll + self.strength + self.add_hit # Вычисление атаки
+        attack_m = self.att # Количество кубов атаки
         add_damage = self.add_damage
         hp = self.hp
         damage = 0
+
         if dice_roll == 1:  # сценарий крит провала
-            damage = randint(1, add_damage)
-            hp -= damage
-            ress = [dice_roll, dice_roll_attack, damage]
+            damage = randint(1, add_damage) # вычисление урона
+            hp -= damage # урон по себе
+            return {'dice_roll':dice_roll, 'dice_roll_attack':dice_roll_attack, 'damage':damage}
         elif dice_roll == 20:  # сценарий крит успеха
-            i = 0
-            while i != attack_m[0]:
+            for i in range(attack_m[0]):
                 damage += randint(1, attack_m[1]) * 2
-                i += 1
             damage += add_damage
-            ress = [dice_roll, dice_roll_attack, damage]
+            return {'dice_roll':dice_roll, 'dice_roll_attack':dice_roll_attack, 'damage':damage}
         else:  # обычкая атака
-            i = 0
-            while i != attack_m[0]:
+            for i in range(attack_m[0]):
                 damage += randint(1, attack_m[1])
-                i += 1
             damage += add_damage
-            ress = [dice_roll, dice_roll_attack, damage]
-        return ress
+            return {'dice_roll':dice_roll, 'dice_roll_attack':dice_roll_attack, 'damage':damage}
